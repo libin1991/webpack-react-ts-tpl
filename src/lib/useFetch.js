a0294857b34feb76296d82ace308c5f871b29248
libin <li.bin_201818@immomo.com>
2022-01-07 19:11:07 +0800

    add more js

--- src/lib/useFetch.js
+++ src/lib/useFetch.js
@@ -1,29 +1,18 @@
-import {
-    useState,
-    useEffect,
-    useRef,
-} from 'react';
+import { useState, useEffect, useRef, } from 'react';
 
 const useFetch = (url, options) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     const [data, setData] = useState(null);
     const abortControllerRef = useRef();
-
     useEffect(() => {
-
         abortControllerRef.current = new AbortController();
-
         setIsLoading(true);
-
         const fetchOptions = options || {};
-
         let timeout;
-
         if (fetchOptions.timeout) {
             timeout = setTimeout(() => abortControllerRef.current.abort(), fetchOptions.timeout);
         }
-
         fetch(url, { ...fetchOptions, signal: abortControllerRef.current.signal })
             .then(res => res.json())
             .then(response => {
@@ -36,16 +25,13 @@ const useFetch = (url, options) => {
                 setIsLoading(false);
                 setError(error);
             })
-
         return () => {
             if (timeout) {
                 clearTimeout(timeout);
             }
-
             abortControllerRef.current.abort();
         }
     }, [url])
-
     return {
         isLoading,
         error,
@@ -53,5 +39,4 @@ const useFetch = (url, options) => {
         abortController: abortControllerRef.current,
     }
 };
-
 export default useFetch;
